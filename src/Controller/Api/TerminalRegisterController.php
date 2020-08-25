dc8933cc8a73a138ec44843af1817be589728bb5
Anatoly Pashin <anatoly.pashin@gmail.com>
2020-08-25 22:02:57 +1000

    ♻️

--- src/Controller/Api/TerminalRegisterController.php
+++ src/Controller/Api/TerminalRegisterController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class TerminalController
+class TerminalRegisterController
 {
     use ParseDataTrait;
     use ValidateEntityTrait;
